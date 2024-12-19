c0dec70bfbc759b0f9d44e4e7135afcd3f6ad469
patelswayam55 <patelswayam9727@gmail.com>
2024-12-19 17:46:11 +0530

    Schema Modificatio

--- models/Blog.js
+++ models/Blog.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const blogSchema = new mongoose.Schema(
   {
@@ -7,12 +7,42 @@ const blogSchema = new mongoose.Schema(
     image: { type: String, required: true },
     createdBy: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true
+      ref: "User",
+      required: true,
     },
-    likeCount: { type: Number, default: 0 }
+    lotSize: { type: Number },
+    startDate: { type: Date },
+    endDate: { type: Date },
+    listingDate: { type: Date },
+    allotmentDate: { type: Date },
+    priceRange: { type: String },
+    type: {
+      type: String,
+      enum: ["Mainboard", "SME"],
+    },
+    marketLot: {
+      retail: {
+        shares: { type: Number },
+        amount: { type: Number },
+      },
+      sHni: {
+        shares: { type: Number },
+        amount: { type: Number },
+      },
+      bHni: {
+        shares: { type: Number },
+        amount: { type: Number },
+      },
+    },
+    totalIssueSize: { type: String },
+    freshIssue: { type: String },
+    offerForSale: { type: String },
+    applyLink: { type: String },
+    faceValue: { type: Number },
+
+    likeCount: { type: Number, default: 0 },
   },
   { timestamps: true }
-)
+);
 
-module.exports = mongoose.model('Blog', blogSchema)
+module.exports = mongoose.model("Blog", blogSchema);
